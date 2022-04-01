a485fd8cf72fefb337f132e59cbf67b7f47cc964
JonathanMerchan <90867139+JonathanMerchan@users.noreply.github.com>
2022-04-01 11:32:51 -0500

    Modelos Paciente y Respuestas

---
+++ Models/Paciente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DigiLearn.Models
+{
+    public class Paciente
+    {
+        public int PacienteId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public string Diagnostico { get; set; }
+        public Guid ProfesionalId { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public bool Estado { get; set; }
+
+    }
+}
---
+++ Models/Respuestas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DigiLearn.Models
+{
+    public class Respuestas
+    {
+        public int RespuestaId { get; set; }
+        public int PacienteId { get; set; }
+        public int Tiempo { get; set; }
+        public int Num_correctas { get; set; }
+        public int Num_preguntas {get;set;}
+        public DateTime Fecha { get; set; }
+
+    }
+}
