64a9c6304e3c47e0a1bf943943ef13dc4ff3d2b0
magustinaperlo <43002670+magustinaperlo@users.noreply.github.com>
2022-04-06 18:00:07 -0300

    cambios en vistas , detalles paciente , index paciente , login , register , botones, tablas , cards

--- Models/Paciente.cs
+++ Models/Paciente.cs
@@ -6,6 +6,7 @@ namespace DigiLearn.Models
     public class Paciente
     {
         [Key]
+   
         public int PacienteId { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
