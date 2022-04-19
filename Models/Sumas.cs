26e472d0a0897897e9cfffedcbc54408bc38a119
magustinaperlo <43002670+magustinaperlo@users.noreply.github.com>
2022-04-19 11:57:42 -0300

    se añade dropdown para asignar actv a cada paciente en la vista detalle, se normaliza la img del login , se añade el boton para redireccionar al listado de pacientes en cada actividad

--- Models/Sumas.cs
+++ Models/Sumas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -18,6 +19,5 @@ namespace DigiLearn.Models
         public int ProfesionalId { get; set; }
         [Required]
         public int PacienteId { get; set; }
-
-    }
+        }
 }
